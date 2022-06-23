8b9eba64124a196ed84756aa1022f3f3fa9fc13d
Jared <jaredterrospin@gmail.com>
2022-06-23 18:12:25 -0400

    added varibles and Mongo Connection

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { Collection } = require('mongodb')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_String,
+    dbName = ''
+collection
 
 
 
