725a50b343eb04c6eacb414647d599687a36843e
Jared <jaredterrospin@gmail.com>
2022-06-23 19:12:56 -0400

    added middleware

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
 let db,
-    dbConnectionString = process.env.DB_String,
+    dbConnectionString = process.env.DB_STRING,
     dbName = 'artistas-latinos',
     collection
 
@@ -15,6 +15,14 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('artistas')
     })
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running`)
